33cb89bc0ad06c270fb4cfbdbd91cf9dc26c3edc
speauty <speauty@163.com>
2019-06-19 14:07:26 +0800

    write json

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -15,19 +15,33 @@ class Router extends AbstractRouter
     $this->setGlobalMode(true);
 
     $this->setMethodNotAllowCallBack(function (Request $request,Response $response){
-        $response->write('the method not found!');
+        $data = Array(
+            "code" => 500,
+            "data" => null,
+            "msg" => 'the method not found!',
+            "time" => time()
+        );
+        $response->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+        $response->withHeader('Content-type', 'application/json;charset=utf-8');
+        $response->withStatus(500);
         return false;//结束此次响应
     });
 
     $this->setRouterNotFoundCallBack(function (Request $request,Response $response){
-        $response->write('the route not found!');
+        $data = Array(
+            "code" => 500,
+            "data" => null,
+            "msg" => 'the route not found!',
+            "time" => time()
+        );
+        $response->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+        $response->withHeader('Content-type', 'application/json;charset=utf-8');
+        $response->withStatus(500);
         return false;//结束此次响应
     });
 
-
     // 拦截GET方法
     $routeCollector->addRoute('GET', '/api_index', '/Api/Index/index');
-
     
   }
 
