32f8e17740ab386b3d198af5262dfc3d18417d6f
speauty <speauty@163.com>
2019-06-19 14:03:25 +0800

    设置路由方法默认方法

--- App/HttpController/Router.php
+++ App/HttpController/Router.php
@@ -14,8 +14,21 @@ class Router extends AbstractRouter
     // 开启全局拦截
     $this->setGlobalMode(true);
 
+    $this->setMethodNotAllowCallBack(function (Request $request,Response $response){
+        $response->write('the method not found!');
+        return false;//结束此次响应
+    });
+
+    $this->setRouterNotFoundCallBack(function (Request $request,Response $response){
+        $response->write('the route not found!');
+        return false;//结束此次响应
+    });
+
+
     // 拦截GET方法
     $routeCollector->addRoute('GET', '/api_index', '/Api/Index/index');
+
+    
   }
 
 
