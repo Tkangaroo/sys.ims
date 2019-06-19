600551d16c31f516819203c2152d4b594433aead
speauty <speauty@163.com>
2019-06-19 17:23:43 +0800

    调整response

--- App/Common.php
+++ App/Common.php
@@ -1,23 +1,35 @@
 <?php
 namespace App;
 
-use EasySwoole\Core\Http\Request;
-use EasySwoole\Core\Http\Response;
-
+use EasySwoole\Http\Request;
+use EasySwoole\Http\Response;
+use EasySwoole\EasySwoole\ServerManager;
 
 class Common
 {
+	$this->response;
+	$this->request;
+
+	protected function request(): Request
+    {
+        return $this->request;
+    }
+
+    protected function response(): Response
+    {
+        return $this->response;
+    }
+
 	public function writeJson($statusCode = 200, $result = null, $msg = null)
 	{
-		$response = new Response(Response $response);
 		$data = Array(
             "code" => $statusCode,
             "data" => $result,
             "msg" => $msg,
             "time" => time()
         );
-        $response->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-        $response->withHeader('Content-type', 'application/json;charset=utf-8');
-        $response->withStatus($statusCode);
+        $this->response()->write(json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+        $this->response()->withHeader('Content-type', 'application/json;charset=utf-8');
+        $this->response()->withStatus($statusCode);
 	}
 }
