0aff0d80f87537f220c13dd25a0f4ae17499c00e
speauty <speauty@163.com>
2019-06-19 12:50:30 +0800

    更新模型

--- App/Model/BaseModel.php
+++ App/Model/BaseModel.php
@@ -1,8 +1,6 @@
 <?php
 namespace App\Model;
 
-use EasySwoole\Mysqli;
-
 class BaseModel
 {
 	
@@ -19,6 +17,6 @@ class BaseModel
     protected function getDb():Mysqli
     {
     	$conf = new \EasySwoole\Mysqli\Config(\EasySwoole\EasySwoole\Config::getInstance()->getConf($this->conf));
-		return new Mysqli($conf);
+		return new \EasySwoole\Mysqli($conf);
     }
 }
