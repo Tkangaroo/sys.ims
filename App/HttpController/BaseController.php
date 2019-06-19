be5af50a046763670220c10c1aa05327d33e2dcc
speauty <speauty@163.com>
2019-06-19 13:29:20 +0800

    修改修饰符

--- App/HttpController/BaseController.php
+++ App/HttpController/BaseController.php
@@ -23,7 +23,7 @@ class BaseController Extends Controller
      * 在准备调用控制器方法处理请求时的事件,如果该方法返回false则不继续往下执行.
 	 * 可用于做控制器基类权限验证等...
      */
-    protected function onRequest(?string $action): ?bool
+    public function onRequest(?string $action): ?bool
 	{
 	   if (parent::onRequest($action)) {
 	        //判断是否登录
@@ -40,7 +40,7 @@ class BaseController Extends Controller
 	 * 当控制器逻辑抛出异常时将调用该方法进行处理异常(框架默认已经处理了异常)
 	 * 可覆盖该方法,进行自定义的异常处理...
 	 */
-    protected function onException(\Throwable $throwable): void
+    public function onException(\Throwable $throwable): void
     {
 		$this->writeJson(200, null, 'connection too much,please wait a moment.');
 	 	return ;
