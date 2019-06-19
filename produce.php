5e37d2a59fc13bd39a924ee253c9639227e64e6d
speauty <speauty@163.com>
2019-06-19 11:24:58 +0800

    更新忽略文件

--- produce.php
+++ produce.php
@@ -26,5 +26,22 @@ return [
     'LOG_DIR' => null,
     'PHAR' => [
         'EXCLUDE' => ['.idea', 'Log', 'Temp', 'easyswoole', 'easyswoole.install']
-    ]
+    ],
+    /*################ MYSQL CONFIG ##################*/
+    'MYSQL' => [
+    //数据库配置
+       'host'                 => '119.23.191.131',//数据库连接ip
+       'user'                 => 'ims',//数据库用户名
+       'password'             => 'catbug',//数据库密码
+       'database'             => 'ims',//数据库
+       'port'                 => '3306',//端口
+       'timeout'              => '30',//超时时间
+       'connect_timeout'      => '5',//连接超时时间
+       'charset'              => 'utf8mb4',//字符编码
+       'strict_type'          => false, //开启严格模式，返回的字段将自动转为数字类型
+       'fetch_mode'           => false,//开启fetch模式, 可与pdo一样使用fetch/fetchAll逐行或获取全部结果集(4.0版本以上)
+       'alias'                => '',//子查询别名
+       'isSubQuery'           => false,//是否为子查询
+       'max_reconnect_times ' => '5',//最大重连次数
+    ],
 ];
