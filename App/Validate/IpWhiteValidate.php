3071fef09f8e82597bbd54317c2eea06e37eb1e1
speauty <speauty@163.com>
2019-06-19 17:33:15 +0800

    调整response

--- App/Validate/IpWhiteValidate.php
+++ App/Validate/IpWhiteValidate.php
@@ -13,11 +13,11 @@ class IpWhiteValidate
 		$flag = false;
 		$valitor = new Validate();
 		if (isset($data['id'])) {
-			$valitor->addColumn('id','主键')->required('不能为空')->numeric('只能为数字类型');
+			$valitor->addColumn('id','主键')->required('主键不能为空')->numeric('主键只能为数字类型');
 		}
-		$valitor->addColumn('ip_addr','IP地址')->required('不能为空')->isIp('无效格式');
-		$valitor->addColumn('is_enable','是否激活')->required('不能为空')->inArray([0,1], true, '无效格式');
-		$valitor->addColumn('comments', '备注')->lengthMax(50, '不能超过50字');
+		$valitor->addColumn('ip_addr','IP地址')->required('IP地址不能为空')->isIp('IP地址无效格式');
+		$valitor->addColumn('is_enable','是否激活')->required('是否激活不能为空')->inArray([0,1], true, '是否激活无效格式');
+		$valitor->addColumn('comments', '备注')->lengthMax(50, '备注不能超过50字');
 		$flag = $valitor->validate($data);
 		var_dump($flag);
 		$msg = $valitor->getError()->getErrorRuleMsg()?:$valitor->getError()->getColumnErrorMsg();
