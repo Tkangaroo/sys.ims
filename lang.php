1adb1d0fbc56feb7ca108cf98d89ad4818f34d04
speauty <speauty@163.com>
2019-06-20 16:23:39 +0800

    '调试配置工具类'

--- lang.php
+++ lang.php
@@ -2,9 +2,11 @@
 
 
 return [
-    'ch' => [
-        'ip_white_not_unique' => 'IP已存在',
-        'ip_white_save_success' => 'IP白名单保存成功',
-        'ip_white_save_error' => 'IP白名单保存失败',
+    'lang' => [
+        'ch' => [
+            'ip_white_not_unique' => 'IP已存在',
+            'ip_white_save_success' => 'IP白名单保存成功',
+            'ip_white_save_error' => 'IP白名单保存失败',
+        ]
     ]
 ];
