81e916cd18317e631326e5892211a742dccf4d55
speauty <speauty@163.com>
2019-06-28 09:54:36 +0800

    plan to hooks

--- App/HttpController/Open/Git.php
+++ App/HttpController/Open/Git.php
@@ -16,7 +16,28 @@ class Git extends BaseController
 {
     public function pull()
     {
-        var_dump($this->request()->getHeaders());
+
+        $osHeaderArgs = $this->getGiteeHeaders();
+        var_dump($osHeaderArgs);
         var_dump($this->request()->getRequestParam());
+        var_dump($this->request()->getBody());
+        var_dump($this->request()->getBody()->__toString());
+        var_dump($this->request()->getParsedBody());
+    }
+
+    /**
+     * to get header args from request of gitee
+     * @return array|null
+     */
+    private function getGiteeHeaders():?array
+    {
+        $headersIdx = [
+            'user-agent',
+            'x-gitee-token',
+            'x-gitee-event'
+        ];
+        $osHeaderArgs = ESTools::getArgFromRequest($this->request(), $headersIdx, 'getHeaders');
+        unset($headersIdx);
+        return $osHeaderArgs;
     }
 }
